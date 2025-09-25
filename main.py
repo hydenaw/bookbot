c0619ee3f39239e1f2b9b50518f5d5074a0d7d61
hydenaw <hydenaw@gmail.com>
2025-09-25 15:16:23 -0400

    word_counter function

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
+def word_counter(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        word_count = len(file_contents.split())
+    return word_count
+
 def main():
-    book_text = get_book_text("/Users/hydena/workspace/github.com/hydenaw/bookbot/books/frankenstein.txt")
-    print(book_text)
+    total_words = word_counter("/Users/hydena/workspace/github.com/hydenaw/bookbot/books/frankenstein.txt")
+    print(total_words)
 main()
