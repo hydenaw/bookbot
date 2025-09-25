40abbb705e934f2d2defa96a383a16b96b8930ae
hydenaw <hydenaw@gmail.com>
2025-09-25 14:58:45 -0400

    First code for main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+def main():
+    book_text = get_book_text("/Users/hydena/workspace/github.com/hydenaw/bookbot/books/frankenstein.txt")
+    print(book_text)
+main()
