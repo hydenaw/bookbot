c28289416a173d2b07c6949d2f04600a10d46338
hydenaw <hydenaw@gmail.com>
2025-09-29 10:59:29 -0400

    Bookbot 9/29

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from stats import character_count
+from stats import character_count, word_counter
 def main():
-    characters = character_count("/Users/hydena/workspace/github.com/hydenaw/bookbot/books/frankenstein.txt")
-    print(characters)
+    file = "books/frankenstein.txt"
+    characters = character_count(file)
+    word_count = word_counter(file)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")      
+    for line in characters:
+        print(line)
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -9,11 +9,25 @@ def word_counter(filepath):
         word_count = len(file_contents.split())
     return word_count
 
+def sort_on(item):
+    return item[1]
+
+import string
+
 def character_count(filepath):
+    allowed_letters = {
+        'e', 't', 'a', 'o', 'i', 'n', 's', 'r', 'h', 'd', 'l', 'm', 'u', 'c', 'f',
+        'y', 'w', 'p', 'g', 'b', 'v', 'k', 'x', 'j', 'q', 'z', 'æ', 'â', 'ê', 'ë', 'ô'
+    }
+    
     with open(filepath) as f:
         file_contents = f.read()
         char_counts = {}
         for char in file_contents:
             char = char.lower()
-            char_counts[char] = char_counts.get(char, 0) +1
-    return char_counts
+            if char in allowed_letters:
+                char_counts[char] = char_counts.get(char, 0) + 1
+                
+    sorted_counts = sorted(char_counts.items(), key=lambda item: item[1], reverse=True)
+    formatted = [f"{char}: {count}" for char, count in sorted_counts]
+    return formatted
