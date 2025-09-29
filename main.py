713b7e2ae544e255c173e445e4712c23347ab665
hydenaw <hydenaw@gmail.com>
2025-09-29 14:07:04 -0400

    Finished Project

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from stats import character_count, word_counter
+import sys
 def main():
-    file = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file = sys.argv[1]
     characters = character_count(file)
     word_count = word_counter(file)
     print("============ BOOKBOT ============")
